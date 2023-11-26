fab514ba862f0d5b2b231e3e09fa0f237210b8e1
Vladyslav Mankivskyi <vladyslav.mankivskyi@gmail.com>
2023-11-26 11:40:07 +0200

    feat: add load env

--- server/app/config.py
+++ server/app/config.py
@@ -1,9 +1,15 @@
+from pydantic_settings import BaseSettings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://user:123@localhost:5432/todo"
 
-engine = create_engine(DATABASE_URL)
+class Settings(BaseSettings):
+    DATABASE_URL: str = "postgresql://user:123@localhost:5432/todo"
+
+
+settings = Settings()
+
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()

14df21cdb53d15597d676473e5bcbf40d211f568
Vladyslav Mankivskyi <vladyslav.mankivskyi@gmail.com>
2023-11-26 11:01:23 +0200

    fix: optional scheme add requirements

--- server/app/schemas.py
+++ server/app/schemas.py
@@ -9,6 +9,6 @@ class TodoSchema(BaseModel):
 
 
 class TodoSchemaOptional(TodoSchema):
-    title: Optional[str] = Field(min_length=1, example="Buy Milk")
-    priority: Optional[int] = Field(gt=0, lt=11, default=5, example=5)
-    complete: Optional[bool] = Field(False, example=False)
+    title: Optional[str] = Field(None, min_length=1, example="Buy Milk")
+    priority: Optional[int] = Field(None, gt=0, lt=11, example=5)
+    complete: Optional[bool] = Field(None, example=False)
